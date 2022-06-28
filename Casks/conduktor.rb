da7a483fdc8791ce19ba31bf8c352a0a3c98ca55
Stéphane Derosiaux <sderosiaux@conduktor.io>
2022-06-29 00:03:49 +0200

    Update conduktor.rb

--- Casks/conduktor.rb
+++ Casks/conduktor.rb
@@ -1,6 +1,6 @@
 cask "conduktor" do
-  version "2.22.0"
-  sha256 "0f51f8262b70bf256d4491bd7730fbadd5b70773d6acab29bb9cdb63e702c17f"
+  version "2.22.1"
+  sha256 "9efdfb5ce83ff529f521bd557c72b9e94ea4c17aecc1b2d4c0d819090a2a7581"
 
   url "https://github.com/conduktor/builds/releases/download/v#{version}/Conduktor-#{version}.pkg", verified: "https://github.com/conduktor"
   name "conduktor"
