12135b8e31f026c67a3385da8c959dd0d63ed166
Stephane Derosiaux <sderosiaux@conduktor.io>
2024-03-14 10:23:54 +0000

    Add Conduktor CLI

---
+++ Casks/conduktor-cli.rb
@@ -0,0 +1,20 @@
+cask "conduktor-cli" do
+  version "0.0.11"
+
+  url "https://github.com/conduktor/ctl/releases/download/#{version}/conduktor-#{version}-darwin-amd64.tar.gz", verified: "https://github.com/conduktor"
+  sha256 "1e44bda75f5893af4fda6104956f618b32cf04a9a29f5c64bf6abd39614b5f37"
+
+  name "Conduktor CLI"
+  desc "Interact with Conduktor with a CLI to perform some operations directly from your command line or a CI/CD pipeline."
+  homepage "https://www.conduktor.io/"
+
+  binary "conduktor"
+
+  test do
+    output = shell_output(bin/"conduktor version")
+    assert output.include? "Version: "
+  end
+
+  #zap trash: "~/.conduktor-cli"
+end
+  
