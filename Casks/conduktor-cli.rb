928b97d98d8d92e1ac14de9c5aa4f79290b97f56
Stéphane Derosiaux <sderosiaux@conduktor.io>
2024-04-01 22:49:01 +0100

    Update conduktor-cli.rb

--- Casks/conduktor-cli.rb
+++ Casks/conduktor-cli.rb
@@ -1,15 +1,3 @@
-cask "conduktor-cli" do
-  version "0.1.0"
-  url "https://github.com/conduktor/ctl/releases/download/#{version}/conduktor-#{version}-darwin-amd64.tar.gz", verified: "https://github.com/conduktor"
-  sha256 "ea1f33a0c09672143c443e19fef7c85b2217ca70623ab72754a118cb8bb5be21"
-
-  name "Conduktor CLI"
-  desc "Interact with Conduktor with a CLI to perform some operations directly from your command line or a CI/CD pipeline."
-  homepage "https://www.conduktor.io/"
-
-  binary "conduktor"
-end
-
 class Conduktorctl < Formula
   name "Conduktor CLI"
   version "0.1.0"
