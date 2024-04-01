fa6e5d3e005b291379b894a616e26f8e041441cc
Stéphane Derosiaux <sderosiaux@conduktor.io>
2024-04-01 23:16:50 +0100

    Fix

--- Formula/conduktor-cli.rb
+++ Formula/conduktor-cli.rb
@@ -1,5 +1,4 @@
-class Conduktorctl < Formula
-  name "Conduktor CLI"
+class ConduktorCli < Formula
   version "0.1.0"
   sha256 "d76471829c0c6493faf0a1f4d7baac85d8da61fc0254cdaf80c76b8bfa76d8f0"
   desc "Conduktor CLI performs operations from your terminal or a CI/CD pipeline"
@@ -8,7 +7,7 @@ class Conduktorctl < Formula
   license "Apache-2.0"
   head "https://github.com/conduktor/ctl.git", branch: "main"
 
-  depends_on formula "go" => :build
+  depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w", output: bin/"conduktor")
