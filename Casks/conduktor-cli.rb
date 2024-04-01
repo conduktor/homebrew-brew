3467e226a5b5f5868d349e0b872d1aba8104a14e
Stéphane Derosiaux <sderosiaux@conduktor.io>
2024-04-01 23:00:09 +0100

    Update conduktor-cli.rb

--- Casks/conduktor-cli.rb
+++ Casks/conduktor-cli.rb
@@ -1,12 +1,12 @@
-cask "conduktor-cli" do
+class Conduktorctl < Formula
   name "Conduktor CLI"
   version "0.1.0"
   sha256 "d76471829c0c6493faf0a1f4d7baac85d8da61fc0254cdaf80c76b8bfa76d8f0"
   desc "Conduktor CLI performs operations from your terminal or a CI/CD pipeline"
   homepage "https://www.conduktor.io/"
   url "https://github.com/conduktor/ctl/archive/refs/tags/#{version}.tar.gz", verified: "https://github.com/conduktor"
-  #license "Apache-2.0"
-  #head "https://github.com/conduktor/ctl.git", branch: "main"
+  license "Apache-2.0"
+  head "https://github.com/conduktor/ctl.git", branch: "main"
 
   depends_on formula "go" => :build
 
