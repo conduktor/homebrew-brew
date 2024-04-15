c6d197794f616e4256e9fc8dde76247b383d0f5c
Stéphane Derosiaux <sderosiaux@conduktor.io>
2024-04-15 13:50:43 +0100

    Update conduktor-cli.rb to 0.1.1

--- Formula/conduktor-cli.rb
+++ Formula/conduktor-cli.rb
@@ -1,6 +1,6 @@
 class ConduktorCli < Formula
-  version "0.1.0"
-  sha256 "d76471829c0c6493faf0a1f4d7baac85d8da61fc0254cdaf80c76b8bfa76d8f0"
+  version "0.1.1"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   desc "Conduktor CLI performs operations from your terminal or a CI/CD pipeline"
   homepage "https://www.conduktor.io/"
   url "https://github.com/conduktor/ctl/archive/refs/tags/#{version}.tar.gz", verified: "https://github.com/conduktor"
