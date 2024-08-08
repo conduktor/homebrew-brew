fd0bb9a3b3340b88fc9dfea5b2c385ad11e54436
Stéphane Derosiaux <sderosiaux@conduktor.io>
2024-08-08 17:13:09 +0100

    Remove the 'v' from the brew version
    Keep in for go

--- Formula/conduktor-cli.rb
+++ Formula/conduktor-cli.rb
@@ -1,10 +1,10 @@
 class ConduktorCli < Formula
-  version "v0.2.7"
+  version "0.2.7"
   sha256 "a4028c78797ce49db0ce8f0a6ac1465c03383a1d5b3e412f73cbc218b077720a"
 
   desc "Conduktor CLI performs operations from your terminal or a CI/CD pipeline"
   homepage "https://www.conduktor.io/"
-  url "https://github.com/conduktor/ctl/archive/refs/tags/#{version}.tar.gz", verified: "https://github.com/conduktor"
+  url "https://github.com/conduktor/ctl/archive/refs/tags/v#{version}.tar.gz", verified: "https://github.com/conduktor"
   license "Apache-2.0"
   head "https://github.com/conduktor/ctl.git", branch: "main"
 
@@ -12,7 +12,7 @@ class ConduktorCli < Formula
 
   def install
     gitSha = "62a00d4807eb7e7d851055e33f50a8a48c376168"
-    system "go", "build", *std_go_args(ldflags: "-s -w -X 'github.com/conduktor/ctl/utils.version=#{version}' -X 'github.com/conduktor/ctl/utils.hash=#{gitSha}'", output: bin/"conduktor")
+    system "go", "build", *std_go_args(ldflags: "-s -w -X 'github.com/conduktor/ctl/utils.version=v#{version}' -X 'github.com/conduktor/ctl/utils.hash=#{gitSha}'", output: bin/"conduktor")
   end
 
   test do
